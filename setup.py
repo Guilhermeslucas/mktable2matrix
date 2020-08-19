342b5e945bfee84bde10be2e1f697c2b5a5c0c8d
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2020-08-19 17:53:26 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="mktable2matrix",
+    version="0.0.1",
+    author="Guilherme Lucas",
+    author_email="guilherme.slucas@gmail.com",
+    description="Converts markdown table to Matrix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/guilhermeslucas/mktable2matrix/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
