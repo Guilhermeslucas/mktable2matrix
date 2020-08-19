e1c2f668e15a5f2112c6ba5bf4eafe51af36159c
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2020-08-19 19:29:47 -0300

    add: version 0.0.8

--- mktable2matrix/mktable2matrix.py
+++ mktable2matrix/mktable2matrix.py
@@ -1,2 +1,13 @@
-def to_matrix():
-    print("I am inside the python package")
+def parse_line(line):
+    splitted_line = line.split('|')
+    return splitted_line
+
+def to_matrix(table_content):
+    matrix_table = []
+    
+    for line in table_content.split('\n')[2:]:
+        matrix_line = parse_line(line)
+        matrix_table.append(matrix_line[1:-1])
+    
+    return matrix_table
+
--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,9 @@
-import mktable2matrix
+import sys
+from mktable2matrix import to_matrix
 
-mktable2matrix.to_matrix()
+f = open('wordsTable.md', 'r')
+
+matrix = to_matrix(f.read())
+
+for line in matrix:
+    print(line)
